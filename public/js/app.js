2ed8e500e952bf454ba41dac5afb12ea1071022a
WesRamox <wesleyramox258@gmail.com>
2024-12-23 00:16:07 -0300

    Responsive

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,6 @@
-const navbar = document.getElementById('navbar');
+const navbar = document.getElementById("navbar");
+const openMenu = document.getElementById("open-menu");
+const menuMobile = document.getElementById("menu-mobile")
 
 window.addEventListener('scroll', () => {
   if (window.scrollY > 300) {
@@ -7,3 +9,8 @@ window.addEventListener('scroll', () => {
     navbar.classList.remove('scroll');
   }
 });
+
+openMenu.addEventListener("click", () => {
+  menuMobile.classList.toggle("open")
+  openMenu.classList.toggle("open")
+})
